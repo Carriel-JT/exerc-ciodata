82ecf4288799c04d1aba6f3128bef2d3a5dd44b4
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2021-03-09 16:54:05 -0300

    Add files via upload
    Exercicio curso OO Python

---
+++ data.py
@@ -0,0 +1,7 @@
+class Data:
+
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+        print("{}/{}/{}".format(self.dia, self.mes, self.ano))
